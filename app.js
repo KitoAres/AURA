8919a8c183d4e8127f2a29a8bfdb1aa0a6daeef0
KittoAres <103236412+KitoAres@users.noreply.github.com>
2026-09-06 22:27:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // --- 1. CONFIGURACIÓN DE SUPABASE ---
 const SUPABASE_URL = 'https://cmlyjfxuybkglkafhyus.supabase.co';
-const SUPABASE_KEY = 'AQUÍ_PEGA_TU_LLAVE_ANON_PUBLIC'; // <--- ¡PEGA TU LLAVE AQUÍ!
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtbHlqZnh1eWJrZ2xrYWZoeXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3NDM2NjUsImV4cCI6MjEwNDMxOTY2NX0.o1z1TMYzJO5VRX5S0dNY2szIrxvntb5m-EyI7QIYOPY'; // <--- ¡PEGA TU LLAVE AQUÍ!
 
 // Inicializar la conexión
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
